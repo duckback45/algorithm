7e9ea07fd978dc55273db4cc4cb52aa313037262
duckback45 <duckback45@gmail.com>
2021-11-28 18:20:36 +0900

    Initial commit

---
+++ src/test/java/stringCompression.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class stringCompression {
+    public int solution(String s){
+
+        return 0;
+    }
+
+    @Test
+    public void _test (){
+        assertEquals(0, solution("aaa"));
+
+    }
+}
